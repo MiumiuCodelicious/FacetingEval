ab96c801d071941174c58f0bce24a6c3f7786a87
ivankacodelicious <ivanka@udel.edu>
2015-04-20 23:35:38 -0400

    Facet statistics

--- src/Faceting/DocFacets.java
+++ src/Faceting/DocFacets.java
@@ -2,6 +2,10 @@ package Faceting;
 
 /**
  * Created by Jewel Li on 15-4-20. ivanka@udel.edu
+ *
+ * DocFacets should take in a ranked list of documents for a particular query
+ * and get all the facets for eaach field in this ranked list.
+ *
  */
 public class DocFacets {
 }
--- src/Faceting/FacetStats.java
+++ src/Faceting/FacetStats.java
@@ -3,7 +3,9 @@ package Faceting;
 /**
  * Created by Jewel Li on 15-4-20. ivanka@udel.edu
  *
- * Statistics about facets.
+ * Statistics about facets in anu given document collection.
+ * This collection should be specified by a given list of document IDs.
+ *
  */
 
 public class FacetStats {
